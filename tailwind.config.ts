2bf59dac008e28b333ac125413938060d5d046f5
prem-web.dev <a70064182@gmail.com>
2026-04-14 06:52:22 +0530

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#0f0f11",
+        foreground: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
